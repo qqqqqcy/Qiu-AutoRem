2267049b781ad79d67755aa2d477a7a6f156b1fe
QQQQQCY <xiajunqcy@163.com>
2018-06-14 23:40:18 +0800

    created autoRem

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# QiuAutoRem
+# Qiu-AutoRem
+通过等比缩放页面元素，适配移动端不同尺寸设备的，js 方案
+
+# 快速使用
+
+```html
+<script src="https://raw.githubusercontent.com/xiajunqcy/QiuAutoRem/master/autoRem.js"></script>
+<script>
+    var autoRem = new AutoRem();
+</script>
+```
+默认比例为：设计图尺寸 750px，fontSize 值为 50px    
+例：750px 的设计稿中，尺寸为 450px x 200px 的元素，css 如下设置即可
+```less
+width: 4.5rem;
+height: 2rem;
+```
+该元素即会跟随页面等比缩放
+
+# 功能说明
+```js
+    var obj = {
+        width: 750, // 设计稿尺寸
+        scale: 100, // 设计稿中 1px 对应css中 1rem 的比例
+    }
+    var autoRem = new AutoRem(obj) // obj 可不传，默认 obj = {width: 750, cale: 100,}
+    autoRem.standFontsize() // 取得当前尺寸页面下的根元素 fontSize px值
+    autoRem.render(obj) // 重新设置 width，scale 并且渲染，obj 不传时只重新渲染
+
+```
--- autoRem.js
+++ autoRem.js
@@ -5,28 +5,45 @@ function AutoRem(obj) {
         this.standard = {}
     }
     this.standard.width = this.standard.width || 375
-    this.standard.fontSize = this.standard.fontSize || 14
+    this.standard.scale = this.standard.scale || 100
+    this.render()
+    this.addListener()
+}
+
+AutoRem.prototype.addListener() = function () {
     var _this = this
-    window.addEventListener('resize',function() {
+    window.addEventListener('resize', function () {
         clearTimeout(this.timer)
-        this.timer = setTimeout(function(){
-            document.querySelectorAll('[auto-src-fin]').forEach(function(el) {
-                el.setAttribute('auto-src', el.getAttribute('auto-src-fin'))
-                el.removeAttribute('auto-src-fin')
-            })
+        this.timer = setTimeout(function () {
+            _this.resetSrc()
             _this.render()
         }, 100)
     });
-    _this.render()
 }
 
-AutoRem.prototype.render = function () {
-    this.standerPx = this.standard.fontSize * (this.limitWidth(window.innerWidth) / this.standard.width) + 'px' //  设置标准单位rem
-    document.querySelector('html').style.fontSize = this.standerPx
+AutoRem.prototype.resetSrc = function () {
+    document.querySelectorAll('[auto-src-fin]').forEach(function (el) {
+        el.setAttribute('auto-src', el.getAttribute('auto-src-fin'))
+        el.removeAttribute('auto-src-fin')
+    })
+}
+
+AutoRem.prototype.standFontsize = function () {
+    var scale = this.standard.scale % 2 ? (this.standard.scale / 2).toFixed(1) : parseInt(this.standard.scale / 2)
+    return scale * (this.limitWidth(window.innerWidth) / this.standard.width) + 'px' //  设置标准单位rem
+}
+
+AutoRem.prototype.render = function (obj) {
+    if ((obj instanceof Object)) {
+        this.standard.width = obj.width || this.standard.width
+        this.standard.scale = obj.scale || this.standard.scale
+    }
+    this.resetSrc()
+    document.querySelector('html').style.fontSize = this.standFontsize()
     this.renderImg()
 }
-AutoRem.prototype.limitWidth = function(n) {
-	return Math.min(Math.max(320, n), 750)
+AutoRem.prototype.limitWidth = function (n) {
+    return Math.min(Math.max(320, n), 720)
 }
 AutoRem.prototype.renderImg = function (el) {
     // el 可以指定元素，不指定的情况下会自动处理页面上所有el
@@ -38,13 +55,13 @@ AutoRem.prototype.renderImg = function (el) {
 
     img.src = imgSrc
     img.onload = function (img) {
-       target.style.backgroundImage = 'url(' + imgSrc + ')'
-       target.style.height = img.target.height * (_this.limitWidth(window.innerWidth) / img.target.width) + 'px'
-       target.style.backgroundSize = 'cover'
-       target.style.boxSizing = 'border-box'
-       target.removeAttribute('auto-src')
-       target.setAttribute('auto-src-fin', imgSrc)
-       if (!el) _this.renderImg()
+        target.style.backgroundImage = 'url(' + imgSrc + ')'
+        target.style.height = img.target.height * (_this.limitWidth(window.innerWidth) / img.target.width) + 'px'
+        target.style.backgroundSize = 'cover'
+        target.style.boxSizing = 'border-box'
+        target.removeAttribute('auto-src')
+        target.setAttribute('auto-src-fin', imgSrc)
+        if (!el) _this.renderImg()
     }
 
 }
